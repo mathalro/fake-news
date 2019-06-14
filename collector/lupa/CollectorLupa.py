b0f9520d828dd900ababf14fb7dcc1179f72cf65
MatheusAlmeidaRosa <matheus.ar11@hotmail.com>
2019-06-14 06:10:22 -0300

    add new colector aos fatos and updating colector lupa

--- collector/lupa/CollectorLupa.py
+++ collector/lupa/CollectorLupa.py
@@ -5,17 +5,22 @@ urlBase = 'https://piaui.folha.uol.com.br/lupa/category/eleicoes-2018/'
 scraper = PagesCollectorLupa(urlBase)
 list_pages = scraper.get_links()
 
-#list_pages = ['https://piaui.folha.uol.com.br/lupa/2018/10/25/debate-doria-franca-globo/', 'https://piaui.folha.uol.com.br/lupa/2018/10/27/tudo-sobre-haddad/']
+#list_pages = ['https://piaui.folha.uol.com.br/lupa/2018/10/24/rj-seis-verdades-paes-witzel/']
 
 news_collector = NewsCollectorLupa()
 
+file = open("data", "a+")
+
 for page in list_pages:
-    print(page)
+    # print(page)
     data = news_collector.get_news(page)
 
     for sample in data:
-        print(sample.content.text)
-        print(sample.description.text)
-        print(sample.label)
-        print()
+        file.write(sample.content.text+"\t"+sample.description.text+"\t"+sample.label+"\n")
+        #print(sample.content.text)
+        #print(sample.description.text)
+        #print(sample.label)
+        #print()
+
+file.close()
         
